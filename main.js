9a0fa1fccce9661d3e5d37689b39acc78a06c889
Felipe Oliveira Nogueira <allexsandroalves77@gmail.com>
2022-08-07 19:40:42 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,195 @@
+var big = false;
+$(document).ready(function(){
+
+
+
+    $('#div1').click(function() {
+        if(big) {
+          big = false;
+          $(this).animate({
+            height : "400px",
+            width : "300px"
+          }, 1000);
+          $("button").show();
+          $("#desc1").hide();
+        } else {
+          big = true;
+          $(this).animate({
+            height : "600px",
+          }, 1000);
+          $("button").hide();
+          $("#desc1").show();
+        }
+    });
+    
+    $('#div2').click(function() {
+        if(big) {
+          big = false;
+          $(this).animate({
+            height : "400px",
+            width : "300px"
+          }, 1000);
+          $("button").show();
+          $("#desc2").hide();
+        } else {
+          big = true;
+          $(this).animate({
+            height : "600px",
+          }, 1000);
+          $("button").hide();
+          $("#desc2").show();
+        }
+    });
+
+    $('#div3').click(function() {
+        if(big) {
+          big = false;
+          $(this).animate({
+            height : "400px",
+            width : "300px"
+          }, 1000);
+          $("button").show();
+          $("#desc3").hide();
+        } else {
+          big = true;
+          $(this).animate({
+            height : "600px",
+          }, 1000);
+          $("button").hide();
+          $("#desc3").show();
+        }
+    });
+
+    $('#div4').click(function() {
+        if(big) {
+          big = false;
+          $(this).animate({
+            height : "400px",
+            width : "300px"
+          }, 1000);
+          $("button").show();
+          $("#desc4").hide();
+        } else {
+          big = true;
+          $(this).animate({
+            height : "600px",
+          }, 1000);
+          $("button").hide();
+          $("#desc4").show();
+        }
+    });
+
+    $('#div5').click(function() {
+        if(big) {
+          big = false;
+          $(this).animate({
+            height : "400px",
+            width : "300px"
+          }, 1000);
+          $("button").show();
+          $("#desc5").hide();
+        } else {
+          big = true;
+          $(this).animate({
+            height : "600px",
+          }, 1000);
+          $("button").hide();
+          $("#desc5").show();
+        }
+    });
+
+    $('#div6').click(function() {
+        if(big) {
+          big = false;
+          $(this).animate({
+            height : "400px",
+            width : "300px"
+          }, 1000);
+          $("button").show();
+          $("#desc6").hide();
+        } else {
+          big = true;
+          $(this).animate({
+            height : "600px",
+          }, 1000);
+          $("button").hide();
+          $("#desc6").show();
+        }
+    });
+
+    $('#div7').click(function() {
+        if(big) {
+          big = false;
+          $(this).animate({
+            height : "400px",
+            width : "300px"
+          }, 1000);
+          $("button").show();
+          $("#desc7").hide();
+        } else {
+          big = true;
+          $(this).animate({
+            height : "600px",
+          }, 1000);
+          $("button").hide();
+          $("#desc7").show();
+        }
+    });
+
+    $('#div8').click(function() {
+        if(big) {
+          big = false;
+          $(this).animate({
+            height : "400px",
+            width : "300px"
+          }, 1000);
+          $("button").show();
+          $("#desc8").hide();
+        } else {
+          big = true;
+          $(this).animate({
+            height : "600px",
+          }, 1000);
+          $("button").hide();
+          $("#desc8").show();
+        }
+    });
+
+    $('#div9').click(function() {
+        if(big) {
+          big = false;
+          $(this).animate({
+            height : "400px",
+            width : "300px"
+          }, 1000);
+          $("button").show();
+          $("#desc9").hide();
+        } else {
+          big = true;
+          $(this).animate({
+            height : "600px",
+          }, 1000);
+          $("button").hide();
+          $("#desc9").show();
+        }
+    });
+
+    $('#div10').click(function() {
+        if(big) {
+          big = false;
+          $(this).animate({
+            height : "400px",
+            width : "300px"
+          }, 1000);
+          $("button").show();
+          $("#desc10").hide();
+        } else {
+          big = true;
+          $(this).animate({
+            height : "600px",
+          }, 1000);
+          $("button").hide();
+          $("#desc10").show();
+        }
+    });
+});
